78c64bf8b01b6d087bcf36461c6c50efa222636c
Beatrice Papi <G00461762@atu.ie>
2026-03-08 20:01:31 +0000

    CONTROLLER package ReservationController and SERVICE package ReservationService

--- src/main/java/atu/ie/week7solution/controller/ReservationController.java
+++ src/main/java/atu/ie/week7solution/controller/ReservationController.java
@@ -1,4 +1,38 @@
 package atu.ie.week7solution.controller;
 
+import atu.ie.week7solution.model.Reservation;
+import atu.ie.week7solution.service.ReservationService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/reservations")
 public class ReservationController {
+
+    private final ReservationService reservationService;
+
+    public ReservationController(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Reservation> addReservation(@Valid @RequestBody Reservation reservation) {
+
+        Reservation created = reservationService.addReservation(reservation);
+        return new ResponseEntity<>(created, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Reservation>> getAllReservations() {
+        return ResponseEntity.ok(reservationService.getAllReservations());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Reservation> getReservationById(@PathVariable Long id) {
+        return ResponseEntity.ok(reservationService.getReservationById(id));
+    }
 }
